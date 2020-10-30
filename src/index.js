c7e2fa3582e71c976c5da3ab27ca942fdc94129d
Calum Scott <calum.scott27@gmail.com>
2020-10-30 16:05:42 +0000

    Added edge case handling and test cases

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 const groupArrayElements = (array, N) => {
-    
-    const noOfArrays = Math.ceil(array.length / N)
-    const currentArr = [...array]
-    const output = []
-  
-    while(currentArr.length > 0) {
-      output.push(currentArr.splice(0, noOfArrays));
-    }
-    return output;
-  }
 
-export default groupArrayElements;
+    if (N < 1) {
+        throw new Error("Divisor must be greater than zero");
+    }
+    else if(array && array.length < 1){
+        throw new Error("Array is empty so cannot be divided");
+    }
+     else if (N === 1) {
+        return array;
+    }
+    else {
+        const noOfArrays = Math.ceil(array.length / N);
+        const currentArr = [...array];
+        const output = [];
+
+        while (currentArr.length > 0) {
+            output.push(currentArr.splice(0, noOfArrays));
+        }
+        return output;
+    }
+}
+
+module.exports = { groupArrayElements };
--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,4 @@
-const { groupArrayElements } = require('./index').default;
+const { groupArrayElements } = require('./index');
 
 describe('index', () => {
 
@@ -13,13 +13,13 @@ describe('index', () => {
         });
 
         test('should return accurate split array length when divisor is 5', () => {
-            
+
             const divisor = 5;
             expect(groupArrayElements(arr, divisor).length).toEqual(5);
         });
 
         test('should return an array length of 3 when divisor is 3 and match expected output', () => {
-           
+
             const divisor = 3;
             const expectedOutput = [[1, 2], [3, 4], [5]];
             const array = [1, 2, 3, 4, 5]
@@ -27,14 +27,35 @@ describe('index', () => {
             expect(groupArrayElements(array, divisor)).toEqual(expectedOutput);
         });
 
+        test('should return an array length of 5 when divisor is 5 and match expected output', () => {
 
-        test('should return an array length of 3 when divisor is 3 and match expected output', () => {
-            
             const divisor = 5;
             const expectedOutput = [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]];
             const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
             expect(groupArrayElements(array, divisor)).toEqual(expectedOutput);
+        });
+
+        test('should throw an error when divisor is less than 1', () => {
+        
+            const divisor = 0;
+
+            try {
+                groupArrayElements(arr, divisor)
+          
+            } catch (e) {
+                expect(e.message).toBe("Divisor must be greater than zero");
+            }
+        });
+
+        test('should throw an error when array is empty', () => {
+            
+            const divisor = 3;
+            try {
+                groupArrayElements([], divisor)
+            } catch (e) {
+                expect(e.message).toBe("Array is empty so cannot be divided");
+            }
         });
     })
 });
